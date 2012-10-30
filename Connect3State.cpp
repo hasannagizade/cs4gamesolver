447ca259c1e0a22a2557e50f1fc3019b654c4ed6
Sol Boucher <slb1566@rit.edu>
2012-10-30 06:49:14 -0400

    Wrote Connect3State and got it compiling
    Even though it is completely untested as yet

---
+++ Connect3State.cpp
@@ -0,0 +1,233 @@
+/** @author Sol Boucher <slb1566@rit.edu> */
+#include "Connect3State.h"
+#include <cassert>
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+#include <utility>
+using namespace std;
+
+/** @brief Board symbols */
+const char SYMBOLS[]={'X', 'O'};
+
+/** @brief Constructor */
+Connect3State::Connect3State( unsigned int columnCount, unsigned int elementCount,
+	const std::vector< std::vector< char > >& original, bool weAreUp ):
+	MY_SYMBOL( weAreUp ? 0 : 1 ), COLUMNS( columnCount ), ELEMENTS( elementCount ),
+	board( original ), ourTurn( weAreUp) 
+{
+	assert( board.size()==COLUMNS );
+	for( vector< vector< char > >::iterator col=board.begin(); col!=board.end();
+		++col )
+		assert( col->size()<=ELEMENTS );
+	
+	finalOutcome=computeWinner();
+	cacheHash();
+}
+
+/** @brief Advancing constructor */
+Connect3State::Connect3State( const Connect3State& baseState, unsigned int column ):
+	MY_SYMBOL( 1-baseState.MY_SYMBOL ),
+	COLUMNS( baseState.COLUMNS ), ELEMENTS( baseState.ELEMENTS ),
+	board( baseState.board ), ourTurn ( !baseState.ourTurn )
+{
+	assert( column<board.size() );
+	board[column].push_back( SYMBOLS[MY_SYMBOL] );
+	assert( board[column].size()<ELEMENTS );
+	
+	finalOutcome=computeWinner( column );
+	cacheHash();
+}
+
+/** @brief Destructor */
+Connect3State::~Connect3State() {}
+
+/** @brief Are we out of objects? */
+bool Connect3State::gameOver() const
+{
+	for( vector< vector< char > >::const_iterator column=board.begin();
+		column!=board.end(); ++column )
+		if( column->size()<ELEMENTS ) return false; //column not complete
+	
+	return finalOutcome!=TIE; //has anyone won?
+}
+
+/** @brief Who won? */
+Connect3State::Score Connect3State::scoreGame() const
+{
+	return finalOutcome;
+}
+
+/** @brief Is it our turn? */
+bool Connect3State::computersTurn() const
+{
+	return ourTurn;
+}
+
+/** @brief What might happen next? */
+const vector< Connect3State > Connect3State::successors() const
+{
+	vector< Connect3State > possibilities;
+	for( unsigned int column=0; column<board.size(); ++column )
+		if( board[column].size()<ELEMENTS-1 )
+			possibilities.push_back( Connect3State( *this, column ) );
+	
+	return possibilities;
+}
+
+/** @brief Textualizes */
+string Connect3State::str() const
+{
+	stringstream assembler;
+	
+	assembler<<"It is the "<<( ourTurn ? "computer" : "human" )<<
+		"'s turn and the board looks like: "<<endl;
+	//print the marks contained in the board:
+	for( int el=ELEMENTS-1; el>=0; --el )
+	{
+		assembler<<PRINTVBAR;
+		for( vector< vector< char > >::const_iterator col=board.begin();
+			col!=board.end();
+			++col )
+			assembler<<( (unsigned)el<col->size() ? ( *col )[el] : PRINTHOLDER )<<PRINTVBAR;
+	}
+	//print the footer below the statie info:
+	assembler<<PRINTFOOTER;
+	for( unsigned int col=0; col<COLUMNS; ++col )
+		assembler<<PRINTFOOTER<<PRINTFOOTER;
+	//print the column numbers:
+	assembler<<' ';
+	for( unsigned int col=0; col<COLUMNS; ++col )
+		assembler<<setw( 2 )<<col;
+	assembler.flush();
+	
+	return assembler.str();
+}
+
+/** @brief Hashing */
+int Connect3State::hash() const
+{
+	return hashCode;
+}
+
+/** @brief Same state? */
+bool Connect3State::operator==( const Connect3State& another ) const
+{
+	if( this->MY_SYMBOL!=another.MY_SYMBOL || this->COLUMNS!=another.COLUMNS ||
+		this->ELEMENTS!=another.ELEMENTS || this->ourTurn!=another.ourTurn ||
+		this->board.size()!=another.board.size() )
+		return false;
+	
+	for( unsigned int group=0; group<COLUMNS; ++group )
+		if( this->board[group].size()!=another.board[group].size() ||
+			!equal( this->board[group].begin(), this->board[group].end(),
+			another.board[group].begin() ) )
+			return false;
+	
+	return true;
+}
+
+/** @brief Assignment */
+Connect3State& Connect3State::operator=( const Connect3State& another )
+{
+	if( this!=&another ) //don't copy over ourself
+	{
+		this->board=another.board;
+		this->ourTurn=another.ourTurn;
+		this->finalOutcome=another.finalOutcome;
+		this->hashCode=another.hashCode;
+	}
+	
+	return *this;
+}
+
+/** @brief Are these subsequent? */
+bool Connect3State::areSubsequent( const Connect3State& first, const Connect3State&
+	next )
+{
+	if( first.MY_SYMBOL==next.MY_SYMBOL || first.COLUMNS!=next.COLUMNS ||
+		first.ELEMENTS!=next.ELEMENTS || first.ourTurn==next.ourTurn ||
+		first.board.size()!=next.board.size() )
+		return false;
+	
+	bool seenDifference=false;
+	for( unsigned int group=0; group<first.COLUMNS; ++group )
+	{
+		if( first.board[group]>next.board[group] ||
+			!equal( first.board[group].begin(), first.board[group].end(),
+			next.board[group].begin() ) )
+			return false;
+		else if( first.board[group]<next.board[group] )
+		{
+			if( !seenDifference ) seenDifference=true;
+			else return false; //more than one move was taken
+		}
+		//else they're exactly the same
+	}
+	
+	return seenDifference;
+}
+
+/** @brief What just happened? */
+int Connect3State::diff( const Connect3State& first, const
+	Connect3State& next )
+{
+	bool subsequentStates=areSubsequent( first, next );
+	
+	assert( subsequentStates );
+	for( unsigned int group=0; group<first.COLUMNS; ++group )
+		if( first.board[group].size()!=next.board[group].size() )
+			return group;
+	
+	assert( true ); //shouldn't have gotten here!
+	return -1;
+}
+
+/** Sorting */
+void Connect3State::cacheHash()
+{
+	hashCode=( ourTurn ? 1 : 0 )<<COLUMNS*ELEMENTS;
+	for( vector< vector< char > >::iterator col=board.begin(); col!=board.end(); ++col )
+		for( vector< char >::iterator el=col->begin(); el!=col->end(); ++el )
+			hashCode+=( *el )<<( ELEMENTS*( col-board.begin() )+( el-col->begin() ) );
+}
+
+/** Compute winner */
+Connect3State::Score Connect3State::computeWinner( int baseCol, int baseEl )
+{
+	if( baseEl==-1 ) baseEl=board[baseCol].size()-1;
+	char match=board[baseCol][baseEl];
+	
+	if( baseCol!=-1 ) //only check things related to the move that was just made
+	{
+		for( int deltaCol=-1; deltaCol<=1; ++deltaCol )
+			for( int deltaEl=-1; deltaEl<=1; ++deltaEl )
+				for( int advance=1; advance<CONNECTABLE; ++advance )
+				{
+					int colIndex=baseCol+deltaCol*advance;
+					int elIndex=baseEl+deltaEl*advance;
+					
+					if( colIndex<0 || (unsigned)colIndex>=board.size() || elIndex<0 || (unsigned)elIndex>=board[colIndex].size() || board[colIndex][elIndex]!=match )
+						break; //give up moving in this direction
+					else if( advance==CONNECTABLE-1 ) //&&board[colIndex][elIndex]==match
+					{
+						if( ourTurn ^ (match!=SYMBOLS[MY_SYMBOL]) )
+							return LOSS;
+						else
+							return VICTORY;
+					}
+					//else we need more matches to make CONNECTABLE of them
+				}
+	}
+	else //check the entire board
+		for( unsigned int col=0; col<board.size(); ++col )
+			for( unsigned int el=0; el<board[col].size(); ++el )
+			{
+				const Score& result=computeWinner( col, el );
+				
+				if( result!=TIE ) return result;
+			}
+	
+	return TIE;
+}
---
+++ Connect3State.h
@@ -0,0 +1,192 @@
+/**
+Represents the Connect-3 game state at some fixed point in time.
+
+@author Sol Boucher <slb1566@rit.edu>
+*/
+#ifndef CONNECT3STATE_H
+#define CONNECT3STATE_H
+
+#include <string>
+#include <utility>
+#include <vector>
+
+class Connect3State
+{
+	public: //gameplay constants
+		/** How many must be connected to win. */
+		static const int CONNECTABLE=3;
+		
+		/** The character that indicates an empty space */
+		static const char PLACEHOLDER='.';
+		
+		/** The characters appearing in the game board */
+		static const char SYMBOLS[];
+		
+		/** The character printed for each empty space */
+		static const char PRINTHOLDER=' ';
+		
+		/** The character separating columns in printout */
+		static const char PRINTVBAR='|';
+		
+		/** The character running along the bottom of the printout */
+		static const char PRINTFOOTER='-';
+		
+		/** The index of this player's symbol, guaranteed in range */
+		const int MY_SYMBOL;
+		
+		/** The major size */
+		const unsigned int COLUMNS;
+		
+		/** The minor size */
+		const unsigned int ELEMENTS;
+		
+	public: //descriptions
+		/** The possible "scores" resulting from a complete match */
+		enum Score
+		{
+			LOSS=-1,
+			TIE=0, //can also mean incomplete
+			VICTORY=1
+		};
+	
+	private: //state
+		/** Stores one pin count per group of pins */
+		std::vector< std::vector< char > > board;
+		
+		/** Whether the computer player is up */
+		bool ourTurn;
+		
+		/** The victor of this particular round. */
+		Score finalOutcome;
+		
+		/** Caches the current hash code */
+		int hashCode;
+	
+	public: //behavior
+		/**
+		Creates a new game given its initial circumstances.
+		@pre <tt>original</tt> is column-major and doesn't exceed bounds
+		@pre <tt>original</tt> contains only elements of SYMBOLS
+		@param columnCount how many columns per board
+		@param elementCount how many elements per column
+		@param original the starting board state
+		@param weAreUp whether or not the "good guy" is up
+		*/
+		Connect3State( unsigned int columnCount,
+			unsigned int elementCount, 
+			const std::vector< std::vector< char > >&
+				original=std::vector< std::vector< char > >(),
+			bool weAreUp=true );
+		
+		/**
+		Creates the move resulting from marking the top of the specifi
+			fied column of the board.
+		@pre the <tt>column</tt> is in range and not yet full
+		@post The new state reflects the fact that it is now the oppos
+			ite player's turn.
+		@param baseState the state on which to base this new one
+		@param column where the new mark is to be placed
+		*/
+		Connect3State( const Connect3State& baseState,
+			unsigned int column );
+		
+		/**
+		Destroys the game state.
+		*/
+		~Connect3State( void );
+		
+		/**
+		Judges whether the game is over.
+		@return whether someone has won or the board is full
+		*/
+		bool gameOver( void ) const;
+		
+		/**
+		Devines the match score, which is only meaningful if the game 
+			is over.
+		@return the score: <tt>Score::VICTORY</tt> for our victory, <t
+			t>Score::LOSS</tt> for opponent's victory, or <tt>Scor
+			e::TIE</tt> for an unterminated game
+		*/
+		Score scoreGame( void ) const;
+		
+		/**
+		Determines whether it is our turn.
+		@return whether the "good guy" is taking his turn
+		*/
+		bool computersTurn( void ) const;
+		
+		/**
+		Returns all possible successor states.
+		@return whatever might happen next
+		*/
+		const std::vector< Connect3State > successors( void ) const;
+		
+		/**
+		Produces a synopsis of this <tt>State</tt>'s particulars.
+		@return the <tt>string</tt> representation
+		*/
+		std::string str( void ) const;
+		
+		/**
+		Hashes the <tt>State</tt>.
+		@pre <tt>hashCode</tt> is up to date
+		@return a hash code 
+		*/
+		int hash( void ) const;
+		
+		/**
+		Checks identity.
+		@pre <tt>sorted</tt> is up to date
+		@param another comparable <tt>State</tt>
+		@return whether the turns and pin groups are the same
+		*/
+		bool operator==( const Connect3State& another ) const;
+		
+		/**
+		Performs assignment.
+		@pre The two instances' constant data match
+		@param another a source <tt>State</tt>
+		@return the destination object
+		*/
+		Connect3State& operator=( const Connect3State& another );
+		
+		/**
+		Determines whether two game states are subsequent.
+		@param first the original state
+		@param next the new state
+		@return whether they would appear in a game in sequence
+		*/
+		static bool areSubsequent( const Connect3State& first,
+			const Connect3State& next );
+		
+		/**
+		Finds the move made to get between two game states.
+		@pre The states must be exactly one move apart.
+		@param first the original state
+		@param next the new state
+		@return the column to which an addition was made
+		*/
+		static int diff( const Connect3State& first,
+			const Connect3State& next );
+	
+	private: //helpers
+		/**
+		Recomputes hash code; must be called every time <tt>pins</tt> is mutated.
+		*/
+		void cacheHash( void );
+		
+		/**
+		Recomputes the game's winner; must be called after mutation.
+		It only checks the entire board if <tt>baseCol</tt> isn't supplied.
+		Otherwise, only combinations including <tt>baseCol</tt> are considered.
+		@pre <tt>baseCol</tt>, if supplied, is positive and in range
+		@pre <tt>baseEl</tt> should normally be omitted, but must be in range
+		@param baseCol the column that was just modified
+		@param baseEl the corresponding element of that column
+		@return a score, with <tt>TIE</tt> representing a nonterminal state
+		*/
+		Score computeWinner( int baseCol=-1, int baseEl=-1 );
+};
+
+#endif
